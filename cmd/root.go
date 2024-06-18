5af8e85381391de4752b464f24e11323ff634806
Ning Shang <syncom.dev@gmail.com>
2024-06-18 14:35:08 -0700

    Add version subcommand (#1)
    The application is still quite barebone, but this is a start.

---
+++ cmd/root.go
@@ -0,0 +1,37 @@
+package psfv
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+	"github.com/syncom/psfv/version"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "psfv",
+	Short: "",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("CLI error: %s", err)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of psfv",
+	Long:  `Print the version of Pitching Speed From Video (psfv)`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf(("Pitching Speed From Video (psfv) v%s\n"), version.Version)
+	},
+}
